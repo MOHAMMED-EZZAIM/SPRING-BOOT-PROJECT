60c7874c9ca414db331ac4e8ce5d018a78961d2e
Mohammed Ezzaim <m.ezzaim2139@uca.ac>
2024-02-22 12:11:12 +0100

    succes!

---
+++ project-spring-sir/src/test/java/usa/harvard/projectspringsir/ProjectSpringSirApplicationTests.java
@@ -0,0 +1,13 @@
+package usa.harvard.projectspringsir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringSirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
